e97ab8c4fc4fc6109796fd51539ac418b0756c84
吴焕楠 <wuhuannan@meizu.com>
2021-01-21 11:41:48 +0800

    版本号统一管理

---
+++ moments/src/main/java/com/nan/wechat/moments/MomentsActivity.kt
@@ -0,0 +1,12 @@
+package com.nan.wechat.moments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MomentsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_moments)
+    }
+}
